985347ee89dddc39d280dea59e9de232646f1a20
TTB-NB-KKJ\TTB KKJ <kj.kim@ttb.kr>
2023-09-07 15:34:04 +0900

    initial

---
+++ src/main/java/com/tt/ttbproject/TtbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tt.ttbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtbProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tt/ttbproject/TtbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.ttbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
